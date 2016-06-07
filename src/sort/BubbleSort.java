26a02beeaee3124d3deba6243d1fa37fa7e303c1
libaitaohong <492563822@qq.com>
2016-06-07 17:16:08 +0800

    冒泡排序

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,21 @@
+package sort;
+
+/**
+ * Created by zhanghe on 2016/6/7
+ * 冒泡排序.
+ */
+public class BubbleSort {
+    public  int[] bubbleSort(int[] A, int n){
+        int temp;
+        for(int j = n; j > 1; j--){
+            for(int i = 0; i < j-1; i++){
+                if (A[i] > A[i+1]){
+                    temp = A[i];
+                    A[i] = A[i+1];
+                    A[i+1] = temp;
+                }
+            }
+        }
+        return A;
+    }
+}

407c87daafc0fcd48e63b6751f453c2009381ea1
libaitaohong <492563822@qq.com>
2016-06-08 09:18:59 +0800

    添加数组为空或长度为1时校验

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -1,18 +1,24 @@
 package sort;
-
 /**
  * Created by zhanghe on 2016/6/7
- * 冒泡排序.
+ * 冒泡排序
+ */
+/**
+ * Created by zhanghe on 2016/6/7
+ * 冒泡排序
  */
 public class BubbleSort {
     public  int[] bubbleSort(int[] A, int n){
         int temp;
+        if(A == null || n <2){
+            return A;
+        }
         for(int j = n; j > 1; j--){
-            for(int i = 0; i < j-1; i++){
-                if (A[i] > A[i+1]){
+            for (int i = 0; i < j - 1; i++) {
+                if (A[i] > A[i + 1]) {
                     temp = A[i];
-                    A[i] = A[i+1];
-                    A[i+1] = temp;
+                    A[i] = A[i + 1];
+                    A[i + 1] = temp;
                 }
             }
         }
